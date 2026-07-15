085b8e86af1e71d80ff5b72a4577b634fb912d48
Dilkhush Kumar <codes.dilkhush@gmail.com>
2026-07-16 03:59:54 +0530

    update nvim plugin

--- nvim/lua/plugins/camouflage.lua
+++ nvim/lua/plugins/camouflage.lua
@@ -1,8 +1,26 @@
 return {
   "zeybek/camouflage.nvim",
-  event = "VeryLazy",
-  opts = {},
+  event = { "BufReadPre", "BufNewFile" },
+
+  opts = {
+    enabled = true,
+    auto_enable = true,
+
+    policy = {
+      enabled = true,
+      default_action = "ignore",
+      rules = {
+        {
+          id = "env-parser-only",
+          action = "mask",
+          parser = { "env" },
+        },
+      },
+    },
+  },
+
   keys = {
     { "<leader>m", "<cmd>CamouflageToggle<cr>", desc = "Toggle Camouflage (Mask Secrets)" },
   },
 }
+
